a6ed21d129565d623a86499195cabbaffd2e1869
Rajat <rajatchoudhary9484@gmail.com>
2023-04-12 23:30:23 +0530

    first commit

---
+++ src/test/java/com/geekster/demo/RandomjokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomjokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
